313d6ab62f8324b74b0846b8aaeb397be22edd6d
Swayam Mukesh Jadhav <swayamj2003@gmail.com>
2025-09-30 15:31:22 +0530

    Add files via upload

---
+++ C_Programming/Assignments/Assignment_No_0/Problem_1.c
@@ -0,0 +1,12 @@
+//Write a C program to add two integers and display the result
+
+void main()
+{
+	int num1,num2,num3;
+	num1=05;
+	num2=10;
+	
+	num3=num1+num2;
+	
+	printf("Addition %d + %d \n= %d",num1,num2,num3);
+}
