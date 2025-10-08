b37e546addda0a9bfaca14e24742c512fb90690d
Swayam Mukesh Jadhav <swayamj2003@gmail.com>
2025-10-08 19:40:37 +0530

    Update Problem_2.c

--- C_Programming/Assignments/Assignment_No_1/Problem_2.c
+++ C_Programming/Assignments/Assignment_No_1/Problem_2.c
@@ -2,28 +2,28 @@
 
 void main()
 {
-	int no = 658;
+	int no = 757;
 	int r1,r2,r3,rev;
 	int q1;
 	
-	r1 = no%10; //8
-//		printf("%d\n",r1);
-	q1 = no/10; //65
+	r1 = no%10; //7
+		printf("%d\n",r1);
+	q1 = no/10; //75
 //		printf("%d\n",q1);
 	r2 = q1%10; //5
-//		printf("%d\n",r2);
-	r3 = q1/10; //6
-//		printf("%d\n",r3);
+		printf("%d\n",r2);
+	r3 = q1/10; //7
+		printf("%d\n",r3);
 
-	rev = r1*100+r2*10+r3;
-//	printf("%d\n Reverse",rev);
+	rev = r1*100 + r2*10 + r3;
+	printf("Reverse = %d\n",rev); //757
 	
 	if(rev == no)
 	{
-		printf("%d\n No is pallindrome");
+		printf("No is pallindrome");
 	}
 	else
 	{
-		printf("%d\n No is Not pallindrome");
+		printf("No is Not pallindrome");
 	}
 }
